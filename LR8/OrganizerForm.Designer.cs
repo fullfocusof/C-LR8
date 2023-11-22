7c1a3fa981331bb037d09c5aad7a709edbaf2696
Max Melnikov <max.vkt@yandex.ru>
2023-11-22 21:29:12 +0300

    Upload

---
+++ LR8/OrganizerForm.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace LR8
+{
+    partial class OrganizerForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.orgTabControl = new System.Windows.Forms.TabControl();
+            this.workTabPage = new System.Windows.Forms.TabPage();
+            this.presonalTabPage = new System.Windows.Forms.TabPage();
+            this.congrTabPage = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.toBuyTabPage = new System.Windows.Forms.TabPage();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.orgTabControl.SuspendLayout();
+            this.workTabPage.SuspendLayout();
+            this.presonalTabPage.SuspendLayout();
+            this.congrTabPage.SuspendLayout();
+            this.toBuyTabPage.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.orgTabControl);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.buttonClose);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonClear);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonDelete);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonChange);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonAdd);
+            this.splitContainer1.Panel2.Controls.Add(this.dateTimePicker1);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxInput);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(684, 361);
+            this.splitContainer1.SplitterDistance = 450;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // orgTabControl
+            // 
+            this.orgTabControl.Controls.Add(this.workTabPage);
+            this.orgTabControl.Controls.Add(this.presonalTabPage);
+            this.orgTabControl.Controls.Add(this.congrTabPage);
+            this.orgTabControl.Controls.Add(this.toBuyTabPage);
+            this.orgTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.orgTabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.orgTabControl.Location = new System.Drawing.Point(0, 0);
+            this.orgTabControl.Name = "orgTabControl";
+            this.orgTabControl.SelectedIndex = 0;
+            this.orgTabControl.Size = new System.Drawing.Size(446, 357);
+            this.orgTabControl.TabIndex = 0;
+            // 
+            // workTabPage
+            // 
+            this.workTabPage.Controls.Add(this.statusStrip);
+            this.workTabPage.Controls.Add(this.listBox1);
+            this.workTabPage.Location = new System.Drawing.Point(4, 25);
+            this.workTabPage.Name = "workTabPage";
+            this.workTabPage.Size = new System.Drawing.Size(438, 328);
+            this.workTabPage.TabIndex = 0;
+            this.workTabPage.Text = "Работа";
+            this.workTabPage.UseVisualStyleBackColor = true;
+            // 
+            // presonalTabPage
+            // 
+            this.presonalTabPage.Controls.Add(this.listBox2);
+            this.presonalTabPage.Location = new System.Drawing.Point(4, 25);
+            this.presonalTabPage.Name = "presonalTabPage";
+            this.presonalTabPage.Size = new System.Drawing.Size(438, 328);
+            this.presonalTabPage.TabIndex = 1;
+            this.presonalTabPage.Text = "Личное";
+            this.presonalTabPage.UseVisualStyleBackColor = true;
+            // 
+            // congrTabPage
+            // 
+            this.congrTabPage.Controls.Add(this.listBox3);
+            this.congrTabPage.Location = new System.Drawing.Point(4, 25);
+            this.congrTabPage.Name = "congrTabPage";
+            this.congrTabPage.Size = new System.Drawing.Size(438, 328);
+            this.congrTabPage.TabIndex = 2;
+            this.congrTabPage.Text = "Поздравить";
+            this.congrTabPage.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(438, 328);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(47, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите сообщение";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInput.Location = new System.Drawing.Point(16, 86);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(200, 22);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxInput_KeyPress);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(16, 10);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(46, 138);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(137, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonChange.Location = new System.Drawing.Point(46, 167);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(137, 23);
+            this.buttonChange.TabIndex = 4;
+            this.buttonChange.Text = "Изменить";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelete.Location = new System.Drawing.Point(46, 196);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(137, 23);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClear.Location = new System.Drawing.Point(46, 267);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(137, 23);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClose.Location = new System.Drawing.Point(46, 324);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(137, 23);
+            this.buttonClose.TabIndex = 7;
+            this.buttonClose.Text = "Закрыть";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(0, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(438, 328);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(0, 0);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(438, 328);
+            this.listBox3.TabIndex = 1;
+            this.listBox3.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // toBuyTabPage
+            // 
+            this.toBuyTabPage.Controls.Add(this.listBox4);
+            this.toBuyTabPage.Location = new System.Drawing.Point(4, 25);
+            this.toBuyTabPage.Name = "toBuyTabPage";
+            this.toBuyTabPage.Size = new System.Drawing.Size(438, 328);
+            this.toBuyTabPage.TabIndex = 3;
+            this.toBuyTabPage.Text = "Купить";
+            this.toBuyTabPage.UseVisualStyleBackColor = true;
+            // 
+            // listBox4
+            // 
+            this.listBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(0, 0);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(438, 328);
+            this.listBox4.TabIndex = 0;
+            this.listBox4.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(112, 17);
+            this.toolStripStatusLabel.Text = "toolStripStatusLabel";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 306);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(438, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // OrganizerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "OrganizerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ежедневник";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OrganizerForm_FormClosing);
+            this.Load += new System.EventHandler(this.OrganizerForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.orgTabControl.ResumeLayout(false);
+            this.workTabPage.ResumeLayout(false);
+            this.workTabPage.PerformLayout();
+            this.presonalTabPage.ResumeLayout(false);
+            this.congrTabPage.ResumeLayout(false);
+            this.toBuyTabPage.ResumeLayout(false);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl orgTabControl;
+        private System.Windows.Forms.TabPage workTabPage;
+        private System.Windows.Forms.TabPage presonalTabPage;
+        private System.Windows.Forms.TabPage congrTabPage;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TabPage toBuyTabPage;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ LR8/OrganizerForm.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LR8
+{
+    public partial class OrganizerForm : Form
+    {
+        string FileName;
+
+        public OrganizerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string tab = "listBox" + (orgTabControl.SelectedIndex + 1);
+            ListBox curBox = (ListBox)Controls.Find(tab, true)[0];
+            if(textBoxInput.Text != string.Empty)
+            {
+                curBox.Items.Add(textBoxInput.Text);
+            }          
+            textBoxInput.Text = string.Empty;
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            string tab = "listBox" + (orgTabControl.SelectedIndex + 1);
+            ListBox curBox = (ListBox)Controls.Find(tab, true)[0];
+            textBoxInput.Text = (string)curBox.SelectedItem;
+        }
+
+        private void OrganizerForm_Load(object sender, EventArgs e)
+        {
+            FileName = dateTimePicker1.Text + "org";
+            LoadFromFile(FileName);
+            toolStripStatusLabel.Text = DateTime.Now.ToString() + " " + DateTime.Now.DayOfWeek;
+        }
+
+        private void SaveToFile(string FileName)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(FileName))
+                {
+                    for (int i = 1; i <= orgTabControl.Controls.Count; i++)
+                    {
+                        ListBox curBox = (ListBox)Controls.Find("listBox" + i, true)[0];
+                        sw.WriteLine(curBox.Items.Count.ToString());
+                        for (int j = 0; j < curBox.Items.Count; j++)
+                        {
+                            sw.WriteLine(curBox.Items[j]);
+                        }
+                        curBox.Items.Clear();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка при сохранении файла", "Ошибка", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadFromFile(string FileName)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(FileName))
+                {
+                    for (int i = 1; i <= orgTabControl.Controls.Count; i++)
+                    {
+                        ListBox curBox = (ListBox)Controls.Find("listBox" + i, true)[0];
+                        int cnt = int.Parse(sr.ReadLine());
+                        for (int j = 0; j < cnt; j++)
+                        {
+                            curBox.Items.Add(sr.ReadLine());
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                for (int i = 1; i <= orgTabControl.Controls.Count; i++)
+                {
+                    ListBox curBox = (ListBox)Controls.Find("listBox" + i, true)[0];
+                    curBox.Items.Clear();
+                }
+                MessageBox.Show("Файл не найден\nСоздание файла успешно", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            SaveToFile(FileName);
+            FileName = dateTimePicker1.Text + "org";
+            LoadFromFile(FileName);
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            string tab = "listBox" + (orgTabControl.SelectedIndex + 1);
+            ListBox curBox = (ListBox)Controls.Find(tab, true)[0];
+            try
+            {
+                curBox.Items[curBox.SelectedIndex] = textBoxInput.Text;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Выберите заметку", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            string tab = "listBox" + (orgTabControl.SelectedIndex + 1);
+            ListBox curBox = (ListBox)Controls.Find(tab, true)[0];
+            curBox.Items.Clear();
+        }
+
+        private void textBoxInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                buttonAdd_Click(sender, e);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel.Text = DateTime.Now.ToString() + " " + DateTime.Now.DayOfWeek;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            string tab = "listBox" + (orgTabControl.SelectedIndex + 1);
+            ListBox curBox = (ListBox)Controls.Find(tab, true)[0];
+            try
+            {
+                int temp = curBox.SelectedIndex;
+                curBox.Items.RemoveAt(curBox.SelectedIndex);
+                if (temp != 0 && temp == curBox.Items.Count)
+                {
+                    curBox.SelectedIndex = temp - 1;
+                }
+                else if(temp == 0 && temp != curBox.Items.Count)
+                {
+                    curBox.SelectedIndex = temp;
+                }              
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Выберите заметку", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void OrganizerForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveToFile(FileName);
+        }
+    }
+}
